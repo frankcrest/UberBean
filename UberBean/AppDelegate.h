2f9bb9db4f8a59623b4261a8c3c5f83fbb34f0d5
Frank Chen <frankcrestview@hotmail.com>
2019-05-17 14:49:07 -0400

    Initial Commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Frank Chen on 2019-05-17.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UberBean
+//
+//  Created by Frank Chen on 2019-05-17.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
